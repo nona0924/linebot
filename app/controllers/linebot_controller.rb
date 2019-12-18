f88e8bca594b414ef60e6aafda14f16c8e29e2ef
EC2 Default User <ec2-user@ip-172-31-28-110.ap-southeast-1.compute.internal>
2019-12-18 06:27:22 +0000

    add to linebot_text

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,59 +1,37 @@
 class LinebotController < ApplicationController
-  require 'line/bot'
-  require 'wikipedia'
+    require 'line/bot'
 
-  # callbackアクションのCSRFトークン認証を無効
-  protect_from_forgery :except => [:callback]
+  def callback
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+    events = client.parse_events_from(body)
 
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+        end
+      end
+      client.reply_message(event['replyToken'], message)
+    end
+    head :ok
+  end
+
+private
+
+# LINE Developers登録完了後に作成される環境変数の認証
   def client
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
-  end
-
-  def callback
-    body = request.body.read
-
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      head :bad_request
-    end
-
-    events = client.parse_events_from(body)
-
-    events.each { |event|
-      if event.message['text'] != nil
-        # LINEで送られてきた文書を取得
-        word = event.message['text']
-        # 日本語版Wikipediaを設定
-        Wikipedia.Configure {
-          domain 'ja.wikipedia.org'
-          path   'w/api.php'
-        }
-      end
-
-      # wikipediaから情報取得
-      page = Wikipedia.find(word)
-
-      # 概要とURLを返す
-      response = page.summary + "\n" + page.fullurl
-
-      case event
-      # メッセージが送信された場合
-      when Line::Bot::Event::Message
-        case event.type
-        # メッセージが送られて来た場合
-        when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: response
-          }
-          client.reply_message(event['replyToken'], message)
-        end
-      end
-    }
-
-    head :ok
   end
 end
